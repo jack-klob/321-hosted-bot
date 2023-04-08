199b75a446eb9e34d026f6c8cebe8dc4f60452b1
jack-klob <98726852+jack-klob@users.noreply.github.com>
2023-04-07 19:36:21 -0700

    passing feature

--- discordBot/features/bot.feature
+++ discordBot/features/bot.feature
@@ -1,6 +1,6 @@
 Feature: Task Creation
 
-    Scenario: Successful task Creation
+    Scenario: Successful task creation
         Given a user
         When the user inputs a command to create a task
         Then the bot responds with a confimation message of the task creation
--- discordBot/features/steps/steps.py
+++ discordBot/features/steps/steps.py
@@ -1,15 +1,22 @@
+import os
 from behave import *
+from dotenv import load_dotenv
+from test_bot import TestBotCommands
+from hamcrest import assert_that, equal_to
 
 @given(u'a user')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a user')
+    load_dotenv()
+    TOKEN = os.environ['TEST_TOKEN']
+    context.commands = TestBotCommands(TOKEN)
 
 
 @when(u'the user inputs a command to create a task')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When the user inputs a command to create a task')
+    context.commands.send_message("!create_task new task")
 
 
 @then(u'the bot responds with a confimation message of the task creation')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then the bot responds with a confimation message of the task creation')
+    message = context.commands.read_reply()['content']
+    assert_that(message, equal_to("The task \"new task\" has been created!"))
