4aba3e3b2defc5907e2e497b1b77ca5bf594aedb
Aidan Watson <aidanwatson@uvic.ca>
2023-04-09 16:13:17 -0700

    GREAT

--- discordBot/features/environment.py
+++ discordBot/features/environment.py
@@ -22,7 +22,7 @@ def before_scenario(context, scenario):
     commands.send_message(f'**Scenario: {scenario.name}**')
 
 
-def after_all(context):
+def after_scenario(contex, scenario):
     APIConnection.delete_all_test_tasks()
     print("Deleted all testing tasks from the database")
     
