ea6dae5ff634b780816dff71528b75756550213c
jack-klob <98726852+jack-klob@users.noreply.github.com>
2023-04-09 00:58:07 -0700

    failing scenario with steps

--- discordBot/features/due_date.feature
+++ discordBot/features/due_date.feature
@@ -4,4 +4,4 @@ Feature: Due Date
         Given a user
         And an id for a created task
         When the user inputs <!due_date <id> "2023-04-06 12:15"> with the id of a task
-        Then the bot responds with "Task <id> due date set to **2023-04-06 12:15**"
+        Then the bot outputs "Task <id> due date set to **2023-04-06 12:15**"
--- discordBot/features/steps/due_date.py
+++ discordBot/features/steps/due_date.py
@@ -5,6 +5,12 @@ from test_utils.test_bot_commands import TestBotCommands
 from hamcrest import assert_that, equal_to
 
 
-@when(u'the user inputs <!due_date <id> "2023-04-06 12:15"> with the id of a task')
+@when(u'the user inputs <!due_date <id> "{date}"> with the id of a task')
+def step_impl(context, date):
+    context.commands.send_message(f'!due_date {context.id} {date}')
+
+@then(u'the bot outputs "Task <id> due date set to **2023-04-06 12:15**"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When the user inputs <!due_date <id> "2023-04-06 12:15"> with the id of a task')
+    reply = context.commands.read_reply()['content']
+    expected = f'Task {context.id} due date set to **2023-04-06 12:15**'
+    assert_that(reply, equal_to(expected))
